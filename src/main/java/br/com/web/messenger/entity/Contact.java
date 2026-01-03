82768cf2c01af911d313ead5630c3cd6042a156a
Gustavo Chaves <gustavosilva712493@gmail.com>
2026-01-02 21:44:19 -0300

    feat: create model to represent contacts between users

---
+++ src/main/java/br/com/web/messenger/entity/Contact.java
@@ -0,0 +1,78 @@
+package br.com.web.messenger.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "contacts", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"user_id", "contact_id"})
+})
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User owner;
+
+    @ManyToOne
+    @JoinColumn(name = "contact_id", nullable = false)
+    private User contact;
+
+    private String nickname;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public Contact() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public User getContact() {
+        return contact;
+    }
+
+    public void setContact(User contact) {
+        this.contact = contact;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    
+}
