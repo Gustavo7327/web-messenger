07a8f79bedc449c2ec017dc8da0b3526b5f77d60
Gustavo Chaves <gustavosilva712493@gmail.com>
2026-01-02 21:45:32 -0300

    feat: create model to generate email codes

---
+++ src/main/java/br/com/web/messenger/entity/EmailCodes.java
@@ -0,0 +1,88 @@
+package br.com.web.messenger.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "email_codes")
+public class EmailCodes {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private Integer code;
+
+    private String token;
+
+    private String type;
+
+    @CreationTimestamp
+    private LocalDateTime expiresAt;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public EmailCodes() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    
+}
