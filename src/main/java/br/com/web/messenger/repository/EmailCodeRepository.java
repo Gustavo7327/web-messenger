886686d5e11fcbbbb21d3ac24a516be35ac421bf
Gustavo Chaves <gustavosilva712493@gmail.com>
2026-01-13 01:07:12 -0300

    feat: create method for find email code

--- src/main/java/br/com/web/messenger/repository/EmailCodeRepository.java
+++ src/main/java/br/com/web/messenger/repository/EmailCodeRepository.java
@@ -1,5 +1,8 @@
 package br.com.web.messenger.repository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,7 @@ import br.com.web.messenger.entity.EmailCode;
 
 @Repository
 public interface EmailCodeRepository extends JpaRepository<EmailCode, Long>{
+
+    Optional<EmailCode> findByTokenAndCodeAndExpiresAtGreaterThan(String token, int code, LocalDateTime now);
     
 }
