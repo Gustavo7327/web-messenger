13f52c089d8e974d746d20fd95401abb83e9e05e
Gustavo Chaves <gustavosilva712493@gmail.com>
2026-01-13 01:12:08 -0300

    feat: create routes for verify email and send email after create account

--- src/main/java/br/com/web/messenger/controller/AuthController.java
+++ src/main/java/br/com/web/messenger/controller/AuthController.java
@@ -3,13 +3,17 @@ package br.com.web.messenger.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.web.messenger.dto.auth.LoginRequest;
 import br.com.web.messenger.dto.auth.LoginResponse;
+import br.com.web.messenger.dto.auth.EmailRequest;
+import br.com.web.messenger.dto.auth.VerifyEmailRequest;
 import br.com.web.messenger.dto.user.UserRegister;
+import br.com.web.messenger.service.EmailCodeService;
 import br.com.web.messenger.service.UserService;
 import br.com.web.messenger.util.ValidationUtils;
 
@@ -20,8 +24,11 @@ public class AuthController {
     
     private final UserService userService;
 
-    public AuthController(UserService userService){
+    private final EmailCodeService emailCodeService;
+
+    public AuthController(UserService userService, EmailCodeService emailCodeService){
         this.userService = userService;
+        this.emailCodeService = emailCodeService;
     }
     
 
@@ -37,6 +44,13 @@ public class AuthController {
         }
 
         userService.createUser(dto);
+
+        try {
+            emailCodeService.createEmailVerificationForEmail(dto.email());
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao enviar email de verificação");
+        }
+
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
@@ -51,5 +65,37 @@ public class AuthController {
         LoginResponse response = userService.generateToken(loginRequest);
 
         return ResponseEntity.ok(response);    
+    }
+
+
+    @PostMapping("/verify-email/{token}")
+    public ResponseEntity<String> verifyEmail(@RequestBody @Valid VerifyEmailRequest body, @PathVariable String token){
+        int code;
+        try {
+            code = Integer.parseInt(body.code().trim());
+        } catch (NumberFormatException ex) {
+            return ResponseEntity.badRequest().body("Formato de código inválido");
+        }
+
+        boolean isVerified = emailCodeService.verifyEmailCode(token, code);
+        if (isVerified) {
+            return ResponseEntity.ok("Email verificado com sucesso");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Código de verificação inválido");
+        }
+    }
+
+
+    @PostMapping("/verify-email/request")
+    public ResponseEntity<String> requestVerification(@RequestBody @Valid EmailRequest body){
+        String email = body.email();
+
+        if (!userService.emailExists(email)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+        }
+
+        emailCodeService.createEmailVerificationForEmail(email);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
