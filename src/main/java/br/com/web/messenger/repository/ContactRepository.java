3ae7eebdf85190569148df66251eb2f81c87beb6
Gustavo Chaves <gustavosilva712493@gmail.com>
2026-05-14 21:24:06 -0300

    feat: changing contact search query

--- src/main/java/br/com/web/messenger/repository/ContactRepository.java
+++ src/main/java/br/com/web/messenger/repository/ContactRepository.java
@@ -1,6 +1,9 @@
 package br.com.web.messenger.repository;
 
+import br.com.web.messenger.dto.contact.ContactResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.web.messenger.entity.Contact;
@@ -10,5 +13,7 @@ import java.util.List;
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long>{
 
-    List<Contact> findAllByOwnerId(Long ownerId);
+    @Query("SELECT new br.com.web.messenger.dto.contact.ContactResponse(c.nickname, c.createdAt, c.id) " +
+            "FROM Contact c WHERE c.owner.email = :email")
+    List<ContactResponse> findContactResponsesByOwnerEmail(@Param("email") String email);
 }
