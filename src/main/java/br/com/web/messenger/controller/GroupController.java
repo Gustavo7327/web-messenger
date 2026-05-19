a95353af2f66d8700a69d810908f7b17d8b342d4
Gustavo Chaves <gustavosilva712493@gmail.com>
2026-05-18 23:44:46 -0300

    feat: create group controller to register the routes

---
+++ src/main/java/br/com/web/messenger/controller/GroupController.java
@@ -0,0 +1,50 @@
+package br.com.web.messenger.controller;
+
+import br.com.web.messenger.dto.group.CreateGroup;
+import br.com.web.messenger.dto.group.GroupDetailsResponse;
+import br.com.web.messenger.dto.group.UpdateGroup;
+import br.com.web.messenger.dto.group.GroupResponse;
+import br.com.web.messenger.service.GroupService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/groups")
+public class GroupController {
+
+    private final GroupService groupService;
+
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+
+    @PostMapping
+    public ResponseEntity<GroupResponse> create(@RequestBody @Valid CreateGroup dto, Authentication authentication) {
+        GroupResponse response = groupService.create(dto, authentication);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GroupResponse> update(
+            @PathVariable Long id,
+            @RequestBody @Valid UpdateGroup dto,
+            Authentication authentication) {
+        GroupResponse response = groupService.update(id, dto, authentication);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GroupResponse> getById(@PathVariable Long id) {
+        GroupResponse response = groupService.getById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}/details")
+    public ResponseEntity<GroupDetailsResponse> getGroupDetails(@PathVariable Long id) {
+        GroupDetailsResponse response = groupService.getGroupDetails(id);
+        return ResponseEntity.ok(response);
+    }
+}
