410802af6f571c0b46ae4cbb4dc6e8d3bda6ebaa
Gustavo Chaves <gustavosilva712493@gmail.com>
2026-05-26 23:16:46 -0300

    feat: create notification repository

---
+++ src/main/java/br/com/web/messenger/repository/NotificationRepository.java
@@ -0,0 +1,15 @@
+package br.com.web.messenger.repository;
+
+import br.com.web.messenger.entity.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    Page<Notification> findByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
+
+    void deleteAllByUserId(Long userId);
+}
