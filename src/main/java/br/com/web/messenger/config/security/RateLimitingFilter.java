629e8bae70221ce51920d7a84134dff4a0a32572
Gustavo Chaves <gustavosilva712493@gmail.com>
2026-06-02 19:02:40 -0300

    feat: add rate limiting to app

---
+++ src/main/java/br/com/web/messenger/config/security/RateLimitingFilter.java
@@ -0,0 +1,44 @@
+package br.com.web.messenger.config.security;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.Duration;
+
+@Component
+public class RateLimitingFilter implements Filter {
+
+    private final StringRedisTemplate redisTemplate;
+    private static final int LIMIT = 10;
+    private static final long WINDOW_DURATION = 60;
+
+    public RateLimitingFilter(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        String clientIp = httpRequest.getRemoteAddr();
+        String key = "rate_limit:" + clientIp;
+        Long requests = redisTemplate.opsForValue().increment(key, 1);
+
+        if (requests == 1) {
+            redisTemplate.expire(key, Duration.ofSeconds(WINDOW_DURATION));
+        }
+
+        if (requests > LIMIT) {
+            httpResponse.setStatus(429);
+            httpResponse.getWriter().write("Too many requests. Please try again later.");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
