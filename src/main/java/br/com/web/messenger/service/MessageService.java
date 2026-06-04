ad7afee2d65a66ac624fba3e6b922955d9e10641
Gustavo Chaves <gustavosilva712493@gmail.com>
2026-06-03 23:47:26 -0300

    feat: create method in service to get unread messages and update the verifications in mark as read method

--- src/main/java/br/com/web/messenger/service/MessageService.java
+++ src/main/java/br/com/web/messenger/service/MessageService.java
@@ -1,10 +1,14 @@
 package br.com.web.messenger.service;
 
 import br.com.web.messenger.entity.Message;
+import br.com.web.messenger.entity.User;
+import br.com.web.messenger.exceptions.ResourceNotFoundException;
+import br.com.web.messenger.repository.jpa.UserRepository;
 import br.com.web.messenger.repository.mongo.MessageRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -15,8 +19,11 @@ public class MessageService {
 
     private final MessageRepository messageRepository;
 
-    public MessageService(MessageRepository messageRepository) {
+    private final UserRepository userRepository;
+
+    public MessageService(MessageRepository messageRepository, UserRepository userRepository) {
         this.messageRepository = messageRepository;
+        this.userRepository = userRepository;
     }
 
 
@@ -37,13 +44,24 @@ public class MessageService {
         return messageRepository.findDirectMessages(userId1, userId2, pageRequest);
     }
 
+
     public Page<Message> getGroupMessagesHistory(Long groupId, int page, int size) {
         PageRequest pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "createdAt"));
         return messageRepository.findByGroupId(groupId, pageRequest);
     }
 
 
-    public void markMessagesAsRead(Long senderId, Long recipientId) {
+    public void markMessagesAsRead(Long senderId, Long recipientId, Authentication authentication) {
+        User user = userRepository.findByEmail(authentication.getName()).orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
+
+        if (user.getId().equals(senderId)) {
+            throw new IllegalArgumentException("Você não pode marcar como lido");
+        }
+
+        if (!user.getId().equals(recipientId)) {
+            throw new IllegalArgumentException("Você não tem permissão de acesso");
+        }
+
         List<Message> unreadMessages = messageRepository.findUnreadMessages(senderId, recipientId);
 
         if (!unreadMessages.isEmpty()) {
@@ -52,7 +70,13 @@ public class MessageService {
         }
     }
 
+
     public long getUnreadCount(Long userId) {
         return messageRepository.countUnreadMessagesByRecipient(userId);
+    }
+
+    public List<Message> getUnreadMessages(Authentication authentication) {
+        User user = userRepository.findByEmail(authentication.getName()).orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
+        return messageRepository.findUnreadMessagesByRecipient(user.getId());
     }
 }
