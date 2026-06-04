8fa74df86c71edc6220ff64401fd571e87c2f123
Gustavo Chaves <gustavosilva712493@gmail.com>
2026-06-03 21:16:20 -0300

    fix: create mongo client to connect the database to app

--- src/main/java/br/com/web/messenger/config/database/DatabaseConfig.java
+++ src/main/java/br/com/web/messenger/config/database/DatabaseConfig.java
@@ -1,11 +1,57 @@
 package br.com.web.messenger.config.database;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "br.com.web.messenger.repository.jpa")
-@EnableMongoRepositories(basePackages = "br.com.web.messenger.repository.mongo")
+@EnableMongoRepositories(
+        basePackages = "br.com.web.messenger.repository.mongo",
+        mongoTemplateRef = "mongoTemplate")
 public class DatabaseConfig {
+
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+
+    @Value("${spring.data.mongodb.password}")
+    private String password;
+
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+
+    @Value("${spring.data.mongodb.port}")
+    private int port;
+
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+
+    @Value("${spring.data.mongodb.authentication-database}")
+    private String authDatabase;
+
+    @Bean
+    public MongoClient mongoClient() {
+        String connectionString = String.format(
+                "mongodb://%s:%s@%s:%d/%s?authSource=%s",
+                username, password, host, port, database, authDatabase
+        );
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .build();
+
+        return MongoClients.create(settings);
+    }
+
+    @Bean(name = "mongoTemplate")
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), database);
+    }
 }
