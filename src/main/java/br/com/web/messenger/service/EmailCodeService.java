2baa82b0c378e1fde95ca9dd40b4f469149cb3c6
Gustavo Chaves <gustavosilva712493@gmail.com>
2026-06-11 00:37:13 -0300

    feat(service): create methods to define logic of forgot password

--- src/main/java/br/com/web/messenger/service/EmailCodeService.java
+++ src/main/java/br/com/web/messenger/service/EmailCodeService.java
@@ -8,8 +8,7 @@ import java.util.Random;
 import br.com.web.messenger.config.rabbitmq.producer.EmailProducer;
 import br.com.web.messenger.constants.EmailType;
 import br.com.web.messenger.dto.email.EmailNotification;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.web.messenger.entity.EmailCode;
@@ -25,17 +24,16 @@ public class EmailCodeService {
     private final EmailCodeRepository emailCodeRepository;
 
     private final UserRepository userRepository;
-    
-    @Autowired
-    private final EmailProducer emailProducer;
-    
-    @Value("${app.base-url:http://localhost:8080}")
-    private String baseUrl;
 
-    public EmailCodeService(EmailCodeRepository emailCodeRepository, UserRepository userRepository, EmailProducer emailProducer) {
+    private final EmailProducer emailProducer;
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public EmailCodeService(EmailCodeRepository emailCodeRepository, UserRepository userRepository, EmailProducer emailProducer, BCryptPasswordEncoder passwordEncoder) {
         this.emailCodeRepository = emailCodeRepository;
         this.userRepository = userRepository;
         this.emailProducer = emailProducer;
+        this.passwordEncoder = passwordEncoder;
     }
 
 
@@ -107,4 +105,36 @@ public class EmailCodeService {
         return true;
     }
 
+
+    public void createPasswordResetEmail(String email) {
+        Optional<User> userOpt = userRepository.findByEmail(email);
+        if (userOpt.isEmpty()) {
+            throw new IllegalArgumentException("User not found for email: " + email);
+        }
+
+        User user = userOpt.get();
+        int code = new Random().nextInt(900000) + 100000;
+        LocalDateTime expiresAt = LocalDateTime.now().plusMinutes(15);
+
+        EmailCode emailCode = new EmailCode(user, code, EmailType.EMAIL_FORGOT_PASSWORD.name(), expiresAt);
+        emailCodeRepository.save(emailCode);
+
+        emailProducer.sendEmailTask(new EmailNotification(email, user.getName(), code, EmailType.EMAIL_FORGOT_PASSWORD));
+    }
+
+
+    public boolean verifyPasswordResetEmailCode(String email, int code, String password) {
+        Optional<EmailCode> opt = emailCodeRepository.findByUserEmailAndCodeAndExpiresAtGreaterThan(email, code, LocalDateTime.now());
+        if (opt.isEmpty()) {
+            return false;
+        }
+
+        EmailCode emailCode = opt.get();
+        User user = emailCode.getUser();
+        user.setPassword(passwordEncoder.encode(password));
+
+        emailCodeRepository.delete(emailCode);
+
+        return true;
+    }
 }
